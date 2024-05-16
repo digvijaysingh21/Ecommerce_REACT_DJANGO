db58102724b3d92efd67e36f7bdea8723b104e73
Digvijay Singh <digvijay.singh@honchominds.com>
2024-05-16 13:54:20 +0530

    added jwt auth

--- backend_api/backend_api/urls.py
+++ backend_api/backend_api/urls.py
@@ -1,9 +1,19 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework_simplejwt import views as jwt_views
+
+# http post http://127.0.0.1:8000/api/token/ username=admin password=admin
+'''
+http post http://127.0.0.1:8000/api/vendors/ "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE1Nzc2NTk2LCJpYXQiOjE3MTU3NzYyOTYsImp0aSI6IjA1MGNjZmU0Mzc1MjQ0NDU5MDhjYWE4ZWYyZGM3Y2Q2IiwidXNlcl9pZCI6MX0.fM0ly9KTOIuC5dOQjmFbPwMBi1kDqJt-2Lc46_-t_vc"
+
+'''
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('main.urls')),
+    path('api/token/',jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/',jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('api-auth/',include('rest_framework.urls')),
+    
 ]
