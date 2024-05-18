2341d0477f38325f758f645b15997af3d002fcba
Digvijay Singh <digvijay.singh@honchominds.com>
2024-05-18 09:39:50 +0530

    setup react frontend and navbar

---
+++ frontend/src/App.js
@@ -0,0 +1,42 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+function App() {
+  return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-secondary">
+        <div className="container">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a className="navbar-brand" href="#">
+              Django-ecommerce
+            </a>
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link " aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Categories
+                </a>
+              </li>
+            </ul>
+            
+          </div>
+        </div>
+      </nav>
+  );
+}
+
+export default App;
